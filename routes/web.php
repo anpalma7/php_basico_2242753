a238576c4400afc42e187735ca2aa215588d8a80
Angie Palma <you@example.comanpalma7@misena.edu.co>
2021-04-27 09:11:28 -0500

    proyecto creado

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("variables" , function(){
+    $mensaje = 20;
+    //Función nativa de php : analizar el contenido de la
+    //variable: tipo de dato, datos contenidos.
+    var_dump($mensaje);
+    echo "<hr />";
+    $mensaje = "Hola 2242753";
+    var_dump($mensaje);
+} );
+
+Route::get("arreglos" , function(){
+    //Definir un arreglos en php
+    //Tamaño: número de elementos de arreglos
+    //Tamaño del arreglo estudiantes es 3.
+    $estudiantes = [ "AN" => "Ana" ,
+                       2  => "Valería" ,
+                     "JO" => "Jorge" ];
+    echo "<pre>";
+    print_r($estudiantes);
+    echo "<pre>";
+
+} );
